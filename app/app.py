2e453765651ff90651ea0623b4f6132d529f7b4e
viochris <140993389+viochris@users.noreply.github.com>
2025-08-03 18:24:05 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -12,13 +12,13 @@ with st.form("prediction_form"):
   name = st.text_input("👤 Patient Name", value="Susy")
 
   pregnancies = st.number_input("🤰 Number of Pregnancies", min_value=0.0, step=1.0, value=6.0)
-  glucose = st.number_input("🍬 Glucose Level", min_value=0.0, value=148.0)
-  blood_pressure = st.number_input("🩸 Blood Pressure", min_value=0.0, value=72.0)
-  skin_thickness = st.number_input("📏 Skin Thickness", min_value=0.0, value=35.0)
-  insulin = st.number_input("💉 Insulin Level", min_value=0.0, value=168.0)
+  glucose = st.number_input("🍬 Glucose Level", min_value=0.0, value=148.0, step=1.0,)
+  blood_pressure = st.number_input("🩸 Blood Pressure", min_value=0.0, value=72.0, step=1.0,)
+  skin_thickness = st.number_input("📏 Skin Thickness", min_value=0.0, value=35.0, step=1.0,)
+  insulin = st.number_input("💉 Insulin Level", min_value=0.0, value=168.0, step=1.0,)
   bmi = st.number_input("⚖️ Body Mass Index (BMI)", min_value=0.0, value=43.1)
   dpf = st.number_input("🧬 Diabetes Pedigree Function (DPF)", min_value=0.0, value=2.288)
-  age = st.number_input("📅 Age", min_value=0.0, max_value=120.0, value=33.0)
+  age = st.number_input("📅 Age", min_value=0.0, max_value=120.0, value=33.0, step=1.0,)
 
   submitted = st.form_submit_button("🚀 Predict")
 
